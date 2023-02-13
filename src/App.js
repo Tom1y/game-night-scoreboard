aaadebc558e8d1815264bc11749aefcd2a85f80f
Tomi <horvat.tomi1@gmail.com>
2023-02-13 13:18:14 +0100

    Added navbar, routing and basic page styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import MyGames from "./Components/MyGames";
+import Home from "./Components/Home";
 
 function App() {
-  return <h1>test</h1>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/myGames" element={<MyGames />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/Components/MyGames.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function MyGames() {
+  return (
+    <div className="myGames">
+      <h2>test2</h2>
+    </div>
+  );
+}
